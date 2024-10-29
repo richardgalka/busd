1b94c6c780331989a1c94a1569dca1c2cc98814e
Richard Galka <richard.galka@gmail.com>
2024-10-29 16:34:04 -0500

    Passenger refactor and movement
    Each passeneger handles their own movement.
    Passenger variables added
    Tweens enabled and called.

--- scripts/external_view.gd
+++ scripts/external_view.gd
@@ -1,34 +1,69 @@
 extends Node2D
 
+
+# Define the range for the number of passengers arriving
+@export var min_passengers: int = 1
+@export var max_passengers: int = 10
+var num_passengers: int = 0
+var num_spawned_passengers: int = 0
+
+# The timer range for when new passengers arrive
+@export var min_timer: float = 1.0
+@export var max_timer: float = 5.0
+
 @export var difficulty : int = 10
-@onready var human_silhoute = $HumanSilhoute
+@onready var scn_human_silhoute: PackedScene = preload("res://busd/Scene/human_silhoute.tscn")
+@onready var marker_2d_lineup = $Marker2D_lineup
+#@onready var marker_2d_lineup = get_node("./Marker2d_lineup")
+#@onready var player_node = get_node("/root/main_scene/Player")
+#@onready var player_node = get_node("/root/main_scene/Player")
+#@onready var player_node = get_node("/root/main_scene/Player")
+
+
 
 var time_left : float = 5.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var vec = Vector2(50,200)
-	_welcome_message()
-	print (_add(3,4))
-	pass # Replace with function body.
+# Preload the HumanSilhoute scene
+var passenger_scene = preload("res://busd/Scene/human_silhoute.tscn")
 
-func _add(a, b):
-	var sum = a+b
-	return sum
-	
+func _ready():
+	# Call the function to start generating passengers
+	#generate_passengers()
+	num_passengers = generate_random_passengers(min_passengers, max_passengers)
+	print("Number of passengers arriving at bus stop: ", num_passengers)
+
+# Function to generate a random number of passengers
+func generate_random_passengers(min: int, max: int) -> int:
+	return randi() % (max - min + 1) + min
+
+# Function to move a passenger along the X axis
+func move_passenger(passenger):
+	passenger.create_tween().tween_property(self, "position:x", marker_2d_lineup.global_position.x-10, 5)
+	print("moving passenger #")
+	var tween = Tween.new()
+	#tween.tween_property(self, "position:x", marker_2d_lineup.global_position.x-10, 5)
+	#add_child(tween)
+	#tween.interpolate_property(passenger, "position:x", passenger.position.x, self.rect_size.x + passenger.rect_size.x, 5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	#tween.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	time_left -= delta
-	#print(time_left)
-	var direction = Vector2(5,1)
-	#print(human_silhoute.global_position)
-	#human_silhoute.global_position += direction*delta
-	pass
+#func _process(delta):
+#	pass
 
-func _welcome_message():
-	print("abba")
-	
+
+func _on_timer_new_person_timeout():
+	print("TIMER")
+	if(num_spawned_passengers < num_passengers):
+		num_spawned_passengers += 1
+		var passenger = passenger_scene.instantiate()
+		passenger.passenger_number = num_spawned_passengers
+		passenger.position = Vector2i(-10, (randi_range(-1, 1) +  marker_2d_lineup.global_position.y))
+		add_child(passenger)
+		passenger.move_passenger()
+		# move passenger
+	else:
+		print("Stopping passenger create timer")
+		$Timer_new_person.stop()
 
 func _unhandled_input(event):
 	if event is InputEventKey:
--- scripts/human_silhoute.gd
+++ scripts/human_silhoute.gd
@@ -1,9 +1,21 @@
 #extends StaticBody2D
 extends Sprite2D
 
-@export var clicks_to_pop : int = 40
+@export var clicks_to_pop : int = 4
 @export var size_increase : float = 0.2
 @export var score_to_give : int = 1
+
+var passenger_fname : String
+var passenger_lname : String
+var passenger_age : int
+var passenger_race : String
+var passenger_number : int
+@export var passenger_width : int = 10
+
+@onready var marker_2d_lineup = get_node("/root/Node2D/ExternalView/Marker2D_lineup")
+
+func _ready():
+	print("test")
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
@@ -14,3 +26,8 @@ func _unhandled_input(event):
 			if clicks_to_pop < 1:
 				#$"/root/Main".increase_score(score_to_give)
 				queue_free()
+
+func move_passenger():
+	print(marker_2d_lineup.position)
+	self.create_tween().tween_property(self, "position:x", marker_2d_lineup.global_position.x-(10+passenger_width*passenger_number), 1.5)
+	print("moving passenger # %s" % passenger_number)
