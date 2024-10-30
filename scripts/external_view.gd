7fe8b187e47dab5fce118dce1489f3fe744cb6b1
Richard Galka <richard.galka@gmail.com>
2024-10-29 23:26:19 -0500

    animation update
    Fix human_silhoute tween and add passenger variables for later use.
    Add bus' animation player to enter scene.

--- scripts/external_view.gd
+++ scripts/external_view.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 
 # Define the range for the number of passengers arriving
+@export_category("Passenger numbers")
 @export var min_passengers: int = 1
 @export var max_passengers: int = 10
 var num_passengers: int = 0
@@ -14,12 +15,7 @@ var num_spawned_passengers: int = 0
 @export var difficulty : int = 10
 @onready var scn_human_silhoute: PackedScene = preload("res://busd/Scene/human_silhoute.tscn")
 @onready var marker_2d_lineup = $Marker2D_lineup
-#@onready var marker_2d_lineup = get_node("./Marker2d_lineup")
-#@onready var player_node = get_node("/root/main_scene/Player")
-#@onready var player_node = get_node("/root/main_scene/Player")
-#@onready var player_node = get_node("/root/main_scene/Player")
-
-
+@onready var bus_animation_player = $BusWindowsseethrough/BusAnimationPlayer
 
 var time_left : float = 5.0
 
@@ -31,6 +27,7 @@ func _ready():
 	#generate_passengers()
 	num_passengers = generate_random_passengers(min_passengers, max_passengers)
 	print("Number of passengers arriving at bus stop: ", num_passengers)
+	bus_animation_player.play("position")
 
 # Function to generate a random number of passengers
 func generate_random_passengers(min: int, max: int) -> int:
@@ -39,17 +36,7 @@ func generate_random_passengers(min: int, max: int) -> int:
 # Function to move a passenger along the X axis
 func move_passenger(passenger):
 	passenger.create_tween().tween_property(self, "position:x", marker_2d_lineup.global_position.x-10, 5)
-	print("moving passenger #")
 	var tween = Tween.new()
-	#tween.tween_property(self, "position:x", marker_2d_lineup.global_position.x-10, 5)
-	#add_child(tween)
-	#tween.interpolate_property(passenger, "position:x", passenger.position.x, self.rect_size.x + passenger.rect_size.x, 5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	#tween.start()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_timer_new_person_timeout():
 	print("TIMER")
--- scripts/human_silhoute.gd
+++ scripts/human_silhoute.gd
@@ -1,21 +1,28 @@
 #extends StaticBody2D
 extends Sprite2D
-
+@export_category("Click Details")
 @export var clicks_to_pop : int = 4
 @export var size_increase : float = 0.2
 @export var score_to_give : int = 1
 
-var passenger_fname : String
-var passenger_lname : String
-var passenger_age : int
-var passenger_race : String
-var passenger_number : int
-@export var passenger_width : int = 10
+@export_category("Passenger Details")
+@export var passenger_fname : String
+@export var passenger_lname : String
+@export var passenger_age : int
+@export var passenger_race : String
+@export var passenger_number : int
+@export_range(0.5, 5) var walk_speed : float = 2.0
+@export_range(2,10) var passenger_width : int = 6
 
 @onready var marker_2d_lineup = get_node("/root/Node2D/ExternalView/Marker2D_lineup")
 
 func _ready():
 	print("test")
+	
+func _process(delta):
+	# As we are low resolution we don't want fractions in our position which can cause tearing
+	self.position = floor(self.position)
+	print(self.position, self.global_position)
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
@@ -29,5 +36,5 @@ func _unhandled_input(event):
 
 func move_passenger():
 	print(marker_2d_lineup.position)
-	self.create_tween().tween_property(self, "position:x", marker_2d_lineup.global_position.x-(10+passenger_width*passenger_number), 1.5)
+	self.create_tween().tween_property(self, "position:x", marker_2d_lineup.global_position.x-(10+passenger_width*passenger_number), walk_speed)
 	print("moving passenger # %s" % passenger_number)
