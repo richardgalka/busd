573eb63ebd01de27d9209e9d4731a05369e55417
Richard Galka <richard.galka@gmail.com>
2024-11-08 22:27:06 -0600

    Randomize Wind

--- scripts/bus_driver_view.gd
+++ scripts/bus_driver_view.gd
@@ -1,12 +1,31 @@
 extends Node2D
 @onready var bus_stop_animation_player = $BusStopAnimationPlayer
 @onready var bus_door_sprite = $BusDoorSprite
+@onready var wind = $Wind
+@onready var wind_animation: AnimationPlayer = $Wind/WindAnimation
+
 
 var door_state_open: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	wind_animation.play_backwards("light_wind")
 	pass
+
+func _process(delta):
+	print(global.bus_state)
+	if bus_stop_animation_player.is_playing():
+		wind_animation.stop
+		global.bus_state = global.BUS_STATE.STOPPED
+		
+	if !wind_animation.is_playing():
+		#randomize wind
+		print("Wind.y before: %s" % wind.global_position)
+		wind.global_position.y = randi_range(125, 360)
+		wind_animation.play_backwards("light_wind")
+		print("Wind.y after: %s" % wind.global_position)
+	
+	
 
 func _on_static_body_2d_input_event(viewport, event:InputEvent, shape_idx):
 	# InputEvent 
---
+++ scripts/global.gd
@@ -0,0 +1,15 @@
+extends Node
+
+enum BUS_STATE {STOPPED, MOVING}
+
+var player_fname: String = "Player"
+var player_lname: String = "One"
+var player_age: int = 22
+var bus_name: String = "Bussy"
+var bus_state: int = BUS_STATE.MOVING
+
+
+
+func _ready():
+	print("I'M FIRST")
+	
