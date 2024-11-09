dbb48359b1c39714b5439b497dcb96b69af14c51
Richard Galka <richard.galka@gmail.com>
2024-11-09 15:45:01 -0600

    Remove AnimationPlayer script
    Remove uneeded animation player script. Update image pointers.

--- scripts/bus_driver_view.gd
+++ scripts/bus_driver_view.gd
@@ -1,5 +1,6 @@
 extends Node2D
 @onready var bus_stop_animation_player = $BusStopAnimationPlayer
+@onready var bus_move_animation_player = $"../ExternalView/Bus/BusMoveAnimationPlayer"
 @onready var bus_door_sprite = $BusDoorSprite
 @onready var wind = $Wind
 @onready var wind_animation: AnimationPlayer = $Wind/WindAnimation
@@ -10,22 +11,25 @@ var door_state_open: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	wind_animation.play_backwards("light_wind")
+	bus_move_animation_player.play("bus_park")
 	pass
 
 func _process(delta):
 	print(global.bus_state)
-	if bus_stop_animation_player.is_playing():
+	if !bus_stop_animation_player.is_playing():
+		#print("bus stopped")
 		wind_animation.stop
 		global.bus_state = global.BUS_STATE.STOPPED
+	animate_wind()
 		
+
+func animate_wind():
 	if !wind_animation.is_playing():
 		#randomize wind
-		print("Wind.y before: %s" % wind.global_position)
+		#print("Wind.y before: %s" % wind.global_position)
 		wind.global_position.y = randi_range(125, 360)
 		wind_animation.play_backwards("light_wind")
-		print("Wind.y after: %s" % wind.global_position)
-	
-	
+		#print("Wind.y after: %s" % wind.global_position)
 
 func _on_static_body_2d_input_event(viewport, event:InputEvent, shape_idx):
 	# InputEvent 
